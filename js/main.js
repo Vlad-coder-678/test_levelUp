b47ce321ebca94391b9f780c47abf951cb319507
Vlad-coder-678 <vova.mezencev@rambler.ru>
2021-03-05 13:21:08 +0300

    add options in timer 'day,days'

--- js/main.js
+++ js/main.js
@@ -1,56 +1,9 @@
 $(function () {
-  // header. slider
+  // slider
 
   $(".slider__inner").slick({
     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
   });
 
-  // timer
-
-  // Set the date we're counting down to
-  let countDownDate = new Date("Apr 01, 2021 0:00:00").getTime();
-
-  // Update the count down every 1 second
-  let clockInterval = setInterval(function () {
-    let now = new Date().getTime();
-    let distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the result in the element
-    let daysEl = document.getElementById("timer__days");
-    let daysEl2 = document.getElementById("timer__days2");
-    let hoursEl = document.getElementById("timer__hours");
-    let minutesEl = document.getElementById("timer__minutes");
-    let secondsEl = document.getElementById("timer__seconds");
-
-    days = days <= 0 ? "0" : days;
-    hours = hours < 10 ? "0" + hours : hours <= 0 ? "0" : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes <= 0 ? "0" : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds <= 0 ? "0" : seconds;
-
-    daysEl.innerHTML = days;
-    daysEl2.innerHTML = days;
-    hoursEl.innerHTML = hours;
-    minutesEl.innerHTML = minutes;
-    secondsEl.innerHTML = seconds;
-
-    // If the count down is finished, write some text
-    if (distance <= 0) {
-      daysEl.innerHTML = "0";
-      hoursEl.innerHTML = "0";
-      minutesEl.innerHTML = "00";
-      secondsEl.innerHTML = "00";
-      clearInterval(clockInterval);
-      document.getElementById("form__timer__text").innerHTML =
-        "Акция завершена";
-    }
-  }, 1000);
 });
